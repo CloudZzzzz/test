6a0f2f20914c09d62d4e1130c375560f1a0cfe39
yun328723@qq.com <yun328723@qq.com>
2019-01-09 16:40:58 +0800

    commit test

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("This is a test!");
         System.out.println("This is the second test!");
+        System.out.print("This is the third test!");
     }
 }
